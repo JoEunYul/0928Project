e6a56dde032332c404bc87ec6393b7f96da1271f
JoEunYul <tjdqhd0464@naver.com>
2021-09-29 16:42:28 +0900

    0928 내용 수정 Push

--- 소스.cpp
+++ 소스.cpp
@@ -2,16 +2,22 @@
 #include <windows.h> //windows 헤더파일 include
 
 using namespace std;
-
-void Input();
-void Process();
+//d엔진 개발자가 구현       
+char Input();
+void Process(char Key);
 void Render();
 
-bool bGameState = true; 
+//개발자가 구현하는 Process
+void CustomProcess(char Key);
+
+//개발자가 구현하는 Render
+void CustomRender();
+
+int Gold = 0;
+
+bool bGameState = true;
 // 꺼짐
-
-char Key;
-
+// ▲▲▲▲▲▲▲▲ 선언 
 int main()
 {   
     // Game engine
@@ -22,27 +28,55 @@ int main()
     while (bGameState) // 끝을 알수 없을때 사용
     {
 
-        Input();
-        Process();
+        char Key = Input();
+        Process(Key);
         Render();
+
     }
 
     return 0;
 }
-void Input()
+
+//    ▽▽▽▽▽▽반환
+char Input() // 엔진 코드
 {
+    //단순히 콘솔 입력
+    //조이스틱, 조이패드
+    // 터치패드, 마우스 등
+    char Key;
+
     cin >> Key;
+
+    return Key;
 }
-void Process()
+void Process(char Key)
 {
+    cout << Key << endl; //log
+
     if (Key == 'q'|| Key == 'Q')
     {
         bGameState = false;  //상단에 while 문에 충족
     }
+
+    if (Key == 'G' || Key == 'g')
+    {
+        Gold++;
+    }
+
 }
 void Render()
 {
     // 프로그램에 사용하지 말것 
     system("cls"); //콘솔창 clear
     cout << "그린다." << endl;
+
+    CustomRender();
+}
+void CustomProcess(char Key)
+{
+   
+}
+void CustomRender()
+{
+    cout << "Gold : " << Gold << endl;
 }
